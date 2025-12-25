6cbc94a3b6c6ef6f4ed4c5053b89847f327e7872
Habib0088 <sarkerbatch2k19@gmail.com>
2025-12-25 11:36:57 +0600

    Nav and footer layout implemented

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='md:w-11/12 mx-auto py-2'>
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
